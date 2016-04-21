8df9b5190fb30e6422a2dcf175c20f1acf22fd75
Zeryther <zeryther@gmail.com>
2016-04-21 20:17:34 +0200

    Initial commit

---
+++ src/main/java/com/zeryther/antiarrowpickup/AAPPlugin.java
@@ -0,0 +1,11 @@
+package com.zeryther.antiarrowpickup;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AAPPlugin extends JavaPlugin {
+
+	public AAPPlugin() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
